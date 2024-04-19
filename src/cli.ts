d139c4cdf4509a47914f3ffdcdd5ed73ccc767bd
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-04-19 08:56:39 +0800

    patch: fix typo on usage of env var

--- src/cli.ts
+++ src/cli.ts
@@ -5,7 +5,7 @@ async function main() {
   await initConfig()
   await scanWeb({
     dir: env.SITE_DIR,
-    listFile: env.SITE_DIR,
+    listFile: env.SITE_FILE_LIST,
     url: env.CLONE_URL,
     scrollInDetail: env.SCROLL_IN_DETAIL == 'true',
   })
